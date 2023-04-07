cb4b444e1a6a8009ea359d20d8cbdb6949682652
Angelo Marchese <marchese.angelo1@gmail.com>
2023-04-07 12:10:15 +0200

    app-group-operator: handle TelemetryService errors

--- app-group-operator/src/main/java/it/unict/AppGroupGraphBuilder.java
+++ app-group-operator/src/main/java/it/unict/AppGroupGraphBuilder.java
@@ -1,13 +1,18 @@
 package it.unict;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class AppGroupGraphBuilder {
+
+    private static final Logger log = LoggerFactory.getLogger(AppGroupGraphBuilder.class);
     
     @RestClient
     TelemetryService telemetryService;
@@ -20,20 +25,33 @@ public class AppGroupGraphBuilder {
               .getMetadata()
               .getLabels()
               .get("app");
+
+            double cpuUsage = 0.0;
+            try {
+                cpuUsage = telemetryService
+                        .getAppCpuUsage(appGroupName, appName)
+                        .await().indefinitely();
+            } catch (Exception e) {
+                log.info(e.getMessage());
+            }
     
-            double cpuUsage = telemetryService
-              .getAppCpuUsage(appGroupName, appName)
-              .await().indefinitely();
+            double memoryUsage = 0.0;
+            try {
+                memoryUsage = telemetryService
+                        .getAppMemoryUsage(appGroupName, appName)
+                        .await().indefinitely();
+            } catch (Exception e) {
+                log.info(e.getMessage());
+            }
     
-            double memoryUsage = telemetryService
-              .getAppMemoryUsage(appGroupName, appName)
-              .await()
-              .indefinitely();
-    
-            Map<String,Double> traffic = telemetryService
-              .getAppTraffic(appGroupName, appName, "all")
-              .await()
-              .indefinitely();
+            Map<String,Double> traffic = new HashMap<>();
+            try {
+                traffic = telemetryService
+                        .getAppTraffic(appGroupName, appName, "all")
+                        .await().indefinitely();
+            } catch (Exception e) {
+                log.info(e.getMessage());
+            }
     
             appGroupGraph.addApp(deployment, cpuUsage, memoryUsage, traffic);
         });
