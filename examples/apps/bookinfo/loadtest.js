be8f97ab70b94f85d2dc43ba07bee3e18cde1f47
Angelo Marchese <marchese.angelo1@gmail.com>
2023-06-12 11:30:51 +0200

    Update examples

--- examples/apps/bookinfo/loadtest.js
+++ examples/apps/bookinfo/loadtest.js
@@ -7,9 +7,9 @@ export const options = {
 };
 
 export default function () {
+  http.get('http://172.28.0.2:30000/productpage');
   http.get('http://172.28.0.3:30000/productpage');
   http.get('http://172.28.0.4:30000/productpage');
   http.get('http://172.28.0.5:30000/productpage');
-  http.get('http://172.28.0.6:30000/productpage');
   sleep(1);
 }
